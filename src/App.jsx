8d346f203d0ccb7a6feebdbed9dba60e1070bb0f
YASH PATEL <144086176+Yash-Patel26@users.noreply.github.com>
2023-09-07 22:07:06 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Yash Patel",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "Yash.patel@studentambassadors.com",
+  gitHub: "https://github.com/Yash-Patel26",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/yash-patel-dgi",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/wood.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "LinkedIn🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+    url: "https://www.linkedin.com/in/yash-patel-dgi",
   },
   {
-    title: "Web Development for Beginners",
+    title: "GitHub",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    url: "https://github.com/Yash-Patel26",
   },
   {
-    title: "My Resume Site",
+    title: "Instagram",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Includes my experience and design abilities.",
+    url: "https://instagram.com/b.techwallah_editor?igshid=MzRlODBiNWFlZA==",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Twitter",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    url: "https://twitter.com/dayj793?t=MUgD27ub3IuYOvmLWMUPuw&s=09",
   },
 ];
 
